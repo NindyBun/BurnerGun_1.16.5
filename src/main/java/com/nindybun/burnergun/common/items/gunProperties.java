63684e27d36c32f855c6449a010cf7229d45da09
NindyBun <69482823+NindyBun@users.noreply.github.com>
2021-12-15 04:49:01 -0800

    working on rendering setting screen

--- src/main/java/com/nindybun/burnergun/common/items/gunProperties.java
+++ src/main/java/com/nindybun/burnergun/common/items/gunProperties.java
@@ -1,6 +1,7 @@
 package com.nindybun.burnergun.common.items;
 
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.nbt.CompoundNBT;
 
 public class gunProperties {
@@ -32,10 +33,27 @@ public class gunProperties {
         CompoundNBT nbt = gun.getOrCreateTag();
         return !nbt.contains(H_EXPAND) ? setHRange(gun, 0) : nbt.getInt(H_EXPAND);
     }
-    //Set or Get Raycase range of Focal Point
+    //Set or Get Raycast range of Focal Point
     public static int setRaycastRange(ItemStack gun, int range){
-
+        gun.getOrCreateTag().putInt(RAYCAST_RANGE, range);
         return range;
+    }
+    public static int getRaycastRange(ItemStack gun){
+        CompoundNBT nbt = gun.getOrCreateTag();
+        return !nbt.contains(RAYCAST_RANGE) ? setRaycastRange(gun, MIN_RAYCAST_RANGE) : nbt.getInt(RAYCAST_RANGE);
+    }
+    //Set or Get Max Raycast range of Focal Point
+    public static int setMaxRaycastRange(ItemStack gun, int range){
+        gun.getOrCreateTag().putInt(MAX_RAYCAST_RANGE, range);
+        return range;
+    }
+    public static int getMaxRaycastRange(ItemStack gun){
+        CompoundNBT nbt = gun.getOrCreateTag();
+        return !nbt.contains(MAX_RAYCAST_RANGE) ? setMaxRaycastRange(gun, MIN_RAYCAST_RANGE) : gun.getOrCreateTag().getInt(MAX_RAYCAST_RANGE);
+    }
+    //Set or Get Whitelist toggle for Trash
+    public static boolean setWhitelist(ItemStack gun, boolean isWhitelist){
+
     }
 
 }
