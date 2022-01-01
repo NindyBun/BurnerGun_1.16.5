f213ce13cca062558269a1328a891c791dc8c7c1
NindyBun <69482823+NindyBun@users.noreply.github.com>
2021-12-31 23:59:58 -0800

    mega blaze summon entity

--- src/main/java/com/nindybun/burnergun/common/entities/MegaBlazeSummonEntity.java
+++ src/main/java/com/nindybun/burnergun/common/entities/MegaBlazeSummonEntity.java
@@ -1,14 +1,26 @@
 package com.nindybun.burnergun.common.entities;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.dispenser.IBlockSource;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
-import net.minecraft.world.DimensionType;
-import net.minecraft.world.LightType;
-import net.minecraft.world.World;
+import net.minecraft.util.math.MutableBoundingBox;
+import net.minecraft.world.*;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.IChunk;
+import net.minecraft.world.gen.feature.StructureFeature;
+import net.minecraft.world.gen.feature.structure.Structure;
+import net.minecraft.world.gen.feature.structure.StructurePiece;
+import net.minecraft.world.gen.feature.structure.StructureStart;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fluids.IFluidBlock;
+
+import java.util.List;
 
 public class MegaBlazeSummonEntity extends ItemEntity {
 
@@ -27,10 +39,26 @@ public class MegaBlazeSummonEntity extends ItemEntity {
 
     @Override
     public void tick() {
-        if (this.isInLava() && this.level.dimension() == World.NETHER){
-            LOGGER.info(this.level);
+        //Chunk chunk = this.level.getChunkAt(this.getOnPos());
+        World world = this.level;
+        Chunk chunk = world.getChunk(this.xChunk, this.zChunk);
+        StructureStart<?> structureStart = chunk.getStartForFeature(Structure.NETHER_BRIDGE);
+        List<StructurePiece> structurePieces = structureStart.getPieces();
+        LOGGER.info(structurePieces);
+        /*if (this.isInLava() && this.level.dimension() == World.NETHER){
+            BlockState state = this.getBlockStateOn();
+            if (state.getFluidState().isSource()){
+                Block north = this.level.getBlockState(this.getOnPos().north()).getBlock();
+                Block west = this.level.getBlockState(this.getOnPos().west()).getBlock();
+                Block east = this.level.getBlockState(this.getOnPos().east()).getBlock();
+                Block south = this.level.getBlockState(this.getOnPos().south()).getBlock();
+                Block bottom = this.level.getBlockState(this.getOnPos().below()).getBlock();
+                if (north == Blocks.NETHER_BRICKS && west == Blocks.NETHER_BRICKS && east == Blocks.NETHER_BRICKS
+                        && south == Blocks.NETHER_BRICKS && bottom == Blocks.NETHER_BRICKS)
+                    LOGGER.info("SUMMONABLE");
+            }
             this.remove();
-        }
+        }*/
         super.tick();
     }
 
